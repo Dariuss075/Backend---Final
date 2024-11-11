6f6a0d441a77163b57b866cd42f3917cf07e2e0a
Dariuss075 <159092794+Dariuss075@users.noreply.github.com>
2024-11-10 23:26:33 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,14 +5,16 @@ const sequelize = new Sequelize(
   process.env.DB_USER,  // Usuario de la base de datos
   process.env.DB_PASS,  // Contraseña de la base de datos
   {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT
+    host: process.env.DB_HOST,  // Dirección del host
+    dialect: process.env.DB_DIALECT,  // Dialecto de la base de datos
+    port: process.env.DB_PORT,  // Puerto de la base de datos
+    dialectOptions: {
+      connectTimeout: 10000,  // Ajustar el tiempo de espera de la conexión si es necesario
+    },
   }
 );
 
 module.exports = sequelize;
-
 
 // Probar la conexión
 sequelize.authenticate()
@@ -20,5 +22,8 @@ sequelize.authenticate()
         console.log("Conexión a la base de datos exitosa!");
     })
     .catch(err => {
+        console.error('Error al conectar a la base de datos', err);
+    });
+
         console.error('Error al conectar a la base de datos', err);
     });
